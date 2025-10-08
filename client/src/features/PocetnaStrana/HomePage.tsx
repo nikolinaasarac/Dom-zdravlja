ff54f201a97dcc4d6dcdb886af33dfbf61f7cfee
Nikolina Sarac <nikolinasarac28@gmail.com>
2025-10-08 17:24:47 +0200

    pocetna stranica

--- client/src/features/PocetnaStrana/HomePage.tsx
+++ client/src/features/PocetnaStrana/HomePage.tsx
@@ -1,13 +1,65 @@
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Grid, Typography } from "@mui/material";
+import "../../styles.css";
+import Kartica from "./Kartica";
+
+const opcije = [
+  {
+    naziv: "Pacijenti",
+    opis: "Prikaži i upravljaj pacijentima",
+    putanja: "/pacijenti",
+    slika: `./../../../images/pacijent.png`,
+  },
+  {
+    naziv: "Pregledi",
+    opis: "Pregledaj evidenciju vakcinacija",
+    putanja: "/pregledi",
+    slika: `./../../../images/pregled.png`,
+  },
+  {
+    naziv: "Zakaži pregled",
+    opis: "Pregledaj evidenciju vakcinacija",
+    putanja: "/",
+    slika: `./../../../images/zakaziPregled.png`,
+  },
+  {
+    naziv: "Moj nalog",
+    opis: "Prikaz statističkih podataka",
+    putanja: "/",
+    slika: "./../../../images/mojNalog.png",
+  },
+  {
+    naziv: "Moj nalog",
+    opis: "Prikaz statističkih podataka",
+    putanja: "/",
+    slika: "./../../../images/mojNalog.png",
+  },
+];
 
 export default function HomePage() {
   return (
-    <>
-      <div>HomePage</div>
-      <Button component={Link} to={`/pacijenti`} variant="outlined">
-        Prikaži pacijente
-      </Button>
-    </>
+    <div className="homepage">
+      <Typography
+        variant="h2"
+        sx={{
+          color: "white",
+          mb: 5,
+        }}
+      >
+        Dobro došli!
+      </Typography>
+      <Grid
+        container
+        spacing={1}
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: "70vh" }} // vertikalno centriranje
+      >
+        {opcije.map((opcija, index) => (
+          <Grid key={index} size={3} display="flex" justifyContent="center">
+            <Kartica kartica={opcija} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   );
 }
