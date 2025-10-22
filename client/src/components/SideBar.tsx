d86116bfda1a3f3cdb571d8d1671de8c6a8e239f
Natasa <natasaelez21@gmail.com>
2025-10-22 12:06:32 +0200

    Popravka

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -21,7 +21,7 @@ export default function Sidebar() {
     { label: "Lični podaci", to: `/pacijenti/${id}`, icon: <svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z" /></svg> },
     { label: "Vakcinacije", to: `/pacijenti/${id}/vakcine`, icon: <svg width="24" height="24" viewBox="0 0 24 24"><path d="M21.71 11.29l-3-3-1.42 1.42 3 3L21.71 11.29zM14.71 18.29l-3-3-1.42 1.42 3 3L14.71 18.29zM2 21h4l9-9-4-4-9 9v4z" /></svg> },
     { label: "Pregledi", to: `/pregledi/${id}`, icon: <svg width="24" height="24" viewBox="0 0 24 24"><path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z" /></svg> },
-    { label: "Karton", to: `/pacijenti/${id}/uputnice`, icon: <svg width="24" height="24" viewBox="0 0 24 24"><path d="M6 2h9l5 5v15c0 1.1-.9 2-2 2H6c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2zm9 5V3.5L18.5 7H15z" /></svg> },
+    { label: "Uputnice", to: `/pacijenti/${id}/uputnice`, icon: <svg width="24" height="24" viewBox="0 0 24 24"><path d="M6 2h9l5 5v15c0 1.1-.9 2-2 2H6c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2zm9 5V3.5L18.5 7H15z" /></svg> },
   ];
 
   return (
